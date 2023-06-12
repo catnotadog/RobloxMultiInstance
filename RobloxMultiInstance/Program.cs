7145942465ea7949cf3b64dcb79e26331af2f961
Justa <Justa@DESKTOP-3HAEFU7>
2023-06-12 10:05:49 -0400

    Add project files.

---
+++ RobloxMultiInstance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RobloxMultiInstance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "funny multiple roblos";
+            new Mutex(true, "ROBLOX_singletonEvent");
+            Console.WriteLine("don, keep open pls");
+            Console.ReadLine();
+        }
+    }
+}
